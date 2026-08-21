c0d108350fde1884c17debff11569c7e003d0ad1
qzhang234 <qzhang234@anl.gov>
2026-08-20 19:47:06 -0500

    backup

--- src/id8_common/devices/sydor_tp4u.py
+++ src/id8_common/devices/sydor_tp4u.py
@@ -8,7 +8,7 @@ capabilities.
 from ophyd import Component
 from ophyd import EpicsSignalRO
 from ophyd import QuadEM
-from ophyd.areadetector.plugins import ImagePlugin_V34
+# from ophyd.areadetector.plugins import ImagePlugin_V34
 from ophyd.areadetector.plugins import StatsPlugin_V34
 from ophyd.quadem import QuadEMPort
 
@@ -30,6 +30,7 @@ class SydorTP4U(QuadEM):
     current3 = Component(StatsPlugin_V34, "Current3:")
     current4 = Component(StatsPlugin_V34, "Current4:")
     # image = Component(ImagePlugin_V34, "image1:")
+    image = None
     sum_all = Component(StatsPlugin_V34, "SumAll:")
 
     # better as text than numbers
